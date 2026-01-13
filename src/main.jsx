12fe2e1429b748f0af6ac3d152e05bfb5f8c62f5
Simon Aquino <simonaquino@gmail.com>
2026-01-13 02:53:23 +0000

    fix: restore BrowserRouter and add 404 hack for GH Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
@@ -8,10 +8,10 @@ import { ErrorBoundary } from './components/ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ErrorBoundary>
         <App />
       </ErrorBoundary>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
